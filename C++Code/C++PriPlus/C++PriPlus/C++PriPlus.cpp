2ea1a5058879e7b0ee29990b1ff98aa93ce814ee
DESKTOP-MSIJKOR\sushare <su1163408001@163.com>
2018-11-07 00:20:39 +0800

    函数指针

--- C++Code/C++PriPlus/C++PriPlus/C++PriPlus.cpp
+++ C++Code/C++PriPlus/C++PriPlus/C++PriPlus.cpp
@@ -2,10 +2,26 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
 
 
 int main()
 {
+
+
+
+#if 1
+	//2018年11月7日
+	//函数指针  & 指针函数
+
+#endif
+
+	printf("Here is end.\n");
+	system("pause");
     return 0;
 }
 
