e35f244cb87231bd01d814751e1d6a1180bf0d65
DESKTOP-MSIJKOR\sushare <su1163408001@163.com>
2018-11-09 23:50:24 +0800

    函数指针的定义 auto的作用很大 但是不靠谱？

--- C++Code/C++PriPlus/C++PriPlus/C++PriPlus.cpp
+++ C++Code/C++PriPlus/C++PriPlus/C++PriPlus.cpp
@@ -6,15 +6,55 @@
 
 using namespace std;
 
+const double* f1(const double ar[], int n);
+const double * f2(const double[], int);
+const double *f3(const double*, int);
 
 
 
 int main()
 {
+#if 1 
+	//2018年11月8日
 
+	double av[3] = { 1112.3, 1542.6, 2227.9 };
 
+	const double *(*p1)(const double*, int) = f1;
+	auto p2 = f2;
+	cout << "address value\n";
+	cout << (*p1)(av, 3) << ":" << *(*p1)(av, 3) << endl;
+	cout << p2(av, 3) << ":" << *p2(av, 3) << endl;
 
-#if 1
+	const double *(*pa[3])(const double *, int) = { f1,f2, f3 };
+	auto pb = pa;
+	cout << "\nUsing an array of pointer to functions:\n";
+	cout << "Address Value\n";
+	for (int i = 0; i < 3; i++)
+	{
+		cout << pa[i](av, 3) << ":" << *pa[i](av, 3) << endl;
+	}
+
+	cout << "\nUsing an pointer to a pointer to a function:\n";
+	cout << "address value\n";
+	for (int i = 0; i < 3; i++)
+	{
+		cout << pb[i](av, 3) << ":" << *pb[i](av, 3) << endl;
+	}
+
+	cout << "\nUsing pointer to an array of pointers:\n";
+	cout << "Address Valus\n";
+	auto pc = &pa;
+	cout << (*pc)[0](av, 3) << ":" << *(*pc)[0](av, 3) << endl;
+
+	const double *(*(*pd)[3])(const double *, int) = &pa;
+
+	const double * pdb = (*pd)[1](av, 3);
+	cout << pdb << ":" << *pdb << endl;
+
+	cout << (*(*pd)[2])(av, 3) << ":" << *(*(*pd)[2])(av, 3) << endl;
+
+#endif
+#if 0
 	//2018年11月7日
 	//函数指针  & 指针函数
 
@@ -25,3 +65,17 @@ int main()
     return 0;
 }
 
+const double *f1(const double *ar, int n)
+{
+	return ar;
+}
+
+const double *f2(const double ar[], int n)
+{
+	return ar + 1;
+}
+
+const double *f3(const double ar[], int n)
+{
+	return ar + 2;
+}
