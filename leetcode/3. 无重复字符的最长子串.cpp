099d3799df3de911501cc8ea3e4d83b54475346f
DESKTOP-MSIJKOR\sushare <su1163408001@163.com>
2020-10-16 16:37:56 +0800

    fix

--- leetcode/3. 无重复字符的最长子串.cpp
+++ leetcode/3. 无重复字符的最长子串.cpp
@@ -1,23 +1,28 @@
 #include "origin.hpp"
 
-class Solution {
+class Solution
+{
 public:
-    int lengthOfLongestSubstring(string s) {
-        if(s.size() == 0) return 0;
+    int lengthOfLongestSubstring(string s)
+    {
+        if (s.size() == 0)
+            return 0;
 
         unordered_set<char> lookup;
 
         int maxstr = 0;
         int left = 0;
 
-        for(int i = 0; i< s.size();i++){
-            while(lookup.find(s[i]) != lookup.end()){
+        for (int i = 0; i < s.size(); i++)
+        {
+            while (lookup.find(s[i]) != lookup.end())
+            {
                 lookup.erase(s[left]);
                 left++;
             }
-            maxstr = max(maxstr,i-left+1);
+            maxstr = max(maxstr, i - left + 1);
             lookup.insert(s[i]);
         }
-            return maxstr;
+        return maxstr;
     }
 };
