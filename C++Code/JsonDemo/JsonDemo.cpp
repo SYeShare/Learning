ea5604a599eaad49f4e8494e11afa37c2a28778b
SYeShare <su1163408001@163.com>
2020-12-05 16:29:55 +0800

    添加文件

--- C++Code/JsonDemo/JsonDemo.cpp
+++ C++Code/JsonDemo/JsonDemo.cpp
@@ -29,17 +29,43 @@ int main()
         j["mt"] = 3;
         j["mtno"] = "su";
 
+        //json子串后加结构体等数据会导致序列化失败
         char jsonbuff[200] = {0};
         string str = "{\"ip\": \"172.16.179.144\", \"type\" : \"MCU_CM_SIMCONFINFO_NTF\"}";
         strncpy(jsonbuff, str.c_str(), str.length());
-
         //json tmp = json::parse(str.c_str());
         cout << json::parse(str.c_str()).dump(4) << endl;
-
         MyTp my;
         memcpy(jsonbuff + str.length(), (char *)&my, sizeof(MyTp));
+        //cout << json::parse(jsonbuff).dump(4) << endl;
 
-        cout << json::parse(jsonbuff).dump(4) << endl;
+        json a;
+        json b;
+        json c;
+        c["name"] = 1;
+        c["sex"] = 2;
+        b["a"].push_back(c);
+
+        c["name"] = 3;
+        c["sex"] = 4;
+        b["a"].push_back(c);
+        cout << b.dump(4) << endl;
+
+        //取值
+        cout << b["a"].size() << endl;
+
+        for (json::iterator j = b["a"].begin(); j != b["a"].end(); j++)
+        {
+            // cout << j["name"].get<int>() << endl;  error
+        }
+        //json构造数组数据
+        //        json jarray;
+        //        jarray["speaker"] = "{{\"mt\": 4, \"mtno\":\"ss\"}, {\"mt\": 5, \"mtno\": \"ll\"}}"_json;
+
+        //        cout << jarray.dump(4) << endl;
+
+        //        string mtid = jarray["speaker"]["mt"].get<string>();
+        //        cout << mtid << endl;
     }
     catch (const std::exception &e)
     {
